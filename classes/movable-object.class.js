1080024fc8a32135de07a23121b0192b93acef97
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-02 16:54:35 +0100

    added character and chicken img

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -3,6 +3,12 @@ class MovableObject {
     y = 400;
     img;
 
+// loadImage('img/test.png')
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
     moveRight() {
         console.log('Moving right');
     }
