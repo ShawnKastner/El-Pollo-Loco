163fcd8c0b6be1e2628853d3cdcfd4963fdeb60b
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-05 15:07:36 +0100

    updated img path

--- classes/character.class.js
+++ classes/character.class.js
@@ -7,11 +7,11 @@ class Character extends MovableObject {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages([
             'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-22.png',
+            'img/2_character_pepe/2_walk/W-23.png',
+            'img/2_character_pepe/2_walk/W-24.png',
+            'img/2_character_pepe/2_walk/W-25.png',
+            'img/2_character_pepe/2_walk/W-26.png',
         ]);
     }
 
