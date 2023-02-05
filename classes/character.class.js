02830a07e3e1f7e89c3f4c43ae6ee05a8d6185d4
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-05 15:06:19 +0100

    added imageCache with character images+

--- classes/character.class.js
+++ classes/character.class.js
@@ -5,6 +5,14 @@ class Character extends MovableObject {
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages([
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-21.png',
+        ]);
     }
 
     jump() {
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -7,6 +7,13 @@ class Chicken extends MovableObject {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
+        // this.animate();
     }
+
+    // animate() {
+    //     setInterval(() => {
+    //         this.x -= 0.25;
+    //     }, 1000 / 60);
+    // }
 
 }
