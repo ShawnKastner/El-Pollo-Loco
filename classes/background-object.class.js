a24ca1acc78c8633629f3608b5861409bd47352e
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-05 14:04:59 +0100

    added air to background

--- classes/background-object.class.js
+++ classes/background-object.class.js
@@ -1,8 +1,8 @@
 class BackgroundObject extends MovableObject {
     width = 720;
-    height = 400;
+    height = 480;
 
-    constructor(imagePath, x, y) {
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
         this.y = 480 - this.height;
