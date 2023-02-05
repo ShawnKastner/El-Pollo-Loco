1c6e3f6a3c1c45fceb7297aafa8b3c40c72bcaad
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-05 15:20:31 +0100

    animated character

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,17 +2,30 @@ class Character extends MovableObject {
     height = 300;
     width = 150;
     y = 130;
+    images_walking = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-22.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png',
-        ]);
+        this.loadImages(this.images_walking);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.images_walking.length; 
+            let path = this.images_walking[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {
