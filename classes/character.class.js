3e96ec6ada751aa208eb22ce304200d232c4e2ab
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-07 16:35:58 +0100

    added jumping images

--- classes/character.class.js
+++ classes/character.class.js
@@ -11,14 +11,30 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
     ];
+
+    images_jumping = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+        'img/2_character_pepe/3_jump/J-40.png',
+    ]
+
     world;
     walking_sound = new Audio('audio/walking.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.images_walking);
+        this.loadImages(this.images_jumping);
 
         this.animate();
+        this.applyGravity();
     }
 
     animate() {
@@ -38,11 +54,17 @@ class Character extends MovableObject {
         }, 1000 / 60);
 
         setInterval(() => {
-            // Walk animation
-            if (this.world.keyboard.RIGHT  || this.world.keyboard.LEFT) {
-                this.playAnimation(this.images_walking);
+
+            if (this.isAboveGround()) {
+                this.playAnimation(this.images_jumping);
+            } else {
+                // Walk animation
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.images_walking);
+                }
             }
         }, 50);
+
     }
 
 
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,39 +8,54 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    accleration = 2.5;
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.accleration;
+            }
+        }, 1000 / 25)
+    }
+
+    isAboveGround() {
+        return this.y < 130;
+    }
 
 // loadImage('img/test.png')
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
+loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+}
 
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+/**
+ * 
+ * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+ */
+loadImages(arr) {
+    arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = img;
+    });
+}
 
-    moveRight() {
-        console.log('Moving right');
-    }
+moveRight() {
+    console.log('Moving right');
+}
 
-    moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
-    }
+moveLeft() {
+    setInterval(() => {
+        this.x -= this.speed;
+    }, 1000 / 60);
+}
 
-    playAnimation() {
-        let i = this.currentImage % this.images_walking.length;
-        let path = this.images_walking[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-    }
+playAnimation() {
+    let i = this.currentImage % this.images_walking.length;
+    let path = this.images_walking[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+}
 }
