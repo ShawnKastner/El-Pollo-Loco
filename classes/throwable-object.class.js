ae48e1d87f4bbfac9efbceb57c907514d46558ac
Shawn Kastner <Shawnkastner@hotmail.de>
2023-02-16 17:09:55 +0100

    play bottleRotation

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,8 +1,15 @@
 class ThrowableObject extends MovableObject {
 
+    bottle_rotation = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
 
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.bottle_rotation);
         this.x = x;
         this.y = y;
         this.height = 100;
@@ -13,8 +20,15 @@ class ThrowableObject extends MovableObject {
      throw() {
         this.speedY = 30;
         this.applyGravity();
+        this.playBottleRotation();
         setInterval(() => {
             this.x += 10;
         }, 25);
+    }
+
+    playBottleRotation() {
+        setInterval(() => {
+            this.playAnimation(this.bottle_rotation);
+        }, 100);
     }
 }
