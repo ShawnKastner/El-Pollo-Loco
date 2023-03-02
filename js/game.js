61a4e496b3d54a5fe7e0aac37ea68844169affef
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-02 11:13:25 +0100

    added fullscreen mode

--- js/game.js
+++ js/game.js
@@ -45,6 +45,30 @@ function stopGame() {
     intervalIds.forEach(clearInterval);
 }
 
+function openFullscreen() {
+    let openFullscreen = document.getElementById('fullscreen');
+    enterFullscreen(openFullscreen);
+}
+
+function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+        element.msRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {  // iOS Safari
+        element.webkitRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+}
+
+
 window.addEventListener("keydown", (e) => {
     if (e.keyCode == 37) {
         keyboard.LEFT = true;
